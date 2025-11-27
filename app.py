ce25768958576c44406e8e758e8121dde3ef422d
BALASUBRAMANI619 <balasubramaninatraj04@gmail.com>
2025-11-27 16:11:54 -0500

    message

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    title = "Home Page"
+    return render_template('home.html', title=title)
+
+if __name__ == "__main__":
+    app.run(debug=True)
