a451bafb63e5e7e6c0d21a6701326c3c85b6ecbd
BALASUBRAMANI619 <balasubramaninatraj04@gmail.com>
2025-11-28 09:45:42 -0500

    added connected app.py to mysql and tested the connection

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
+
+# ---------- MySQL Database Configuration ----------
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:new_password@localhost/flask_crud"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:your_password@localhost/flask_crud"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+with app.app_context():
+    try:
+        result = db.session.execute(text("SELECT 1")).scalar()
+        print("Connection successful:", result)
+    except Exception as e:
+        print("Connection failed:", e)
 
 @app.route("/")
 def home():
